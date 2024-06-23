52a90064d3bcd875e6ac9997287e8a8a7d5c050b
Subahan-1D <subahanislam523@gmail.com>
2024-06-24 00:04:10 +0600

    Auth Provider file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/images/logo.png'
 import { AuthContext } from '../provider/AuthProvider'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
-    const { user, logOut } = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     return (
         <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
             <div className='flex-1'>
@@ -15,59 +15,51 @@ const Navbar = () => {
             <div className='flex-none'>
                 <ul className='menu menu-horizontal px-1'>
                     <li>
-                        <Link>Home</Link>
+                        <Link to='/'>Home</Link>
                     </li>
-                    {/* conditional rendering */}
-                    {
-                        !user &&
+                    {!user &&
                         <li>
                             <Link to='/login'>Login</Link>
                         </li>
                     }
                 </ul>
-
                 {/* conditional rendering */}
-                {
-                    user && (
-                        <div className='dropdown dropdown-end z-50'>
-                            <div
-                                tabIndex={0}
-                                role='button'
-                                className='btn btn-ghost btn-circle avatar'
-                            >
-                                <div className='w-10 rounded-full' title={user?.displayName}>
-                                    <img
-                                        referrerPolicy='no-referrer'
-                                        alt='User Profile Photo'
-                                        src={user?.photoURL}
-                                    />
-                                </div>
-                            </div>
-                            <ul
-                                tabIndex={0}
-                                className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
-                            >
-                                <li>
-                                    <Link to='/add-job' className='justify-between'>Add Job</Link>
-                                </li>
-                                <li>
-                                    <Link to='/my-posted-jobs'>My Posted Jobs</Link>
-                                </li>
-                                <li>
-                                    <Link to='/my-bids'>My Bids</Link>
-                                </li>
-                                <li>
-                                    <Link to='/bid-request'>Bid Requests</Link>
-                                </li>
-                                <li className='mt-2'>
-                                    <button onClick={logOut}
-                                        className='bg-gray-200 block text-center'>Logout</button>
-                                </li>
-                            </ul>
-                        </div>
-                    )
-                }
 
+                {user && <div className='dropdown dropdown-end z-50'>
+                    <div
+                        tabIndex={0}
+                        role='button'
+                        className='btn btn-ghost btn-circle avatar'
+                    >
+                        <div className='w-10 rounded-full' title=''>
+                            <img
+                                referrerPolicy='no-referrer'
+                                alt='User Profile Photo'
+                                src=''
+                            />
+                        </div>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+                    >
+                        <li>
+                            <div className='justify-between'>Add Job</div>
+                        </li>
+                        <li>
+                            <div>My Posted Jobs</div>
+                        </li>
+                        <li>
+                            <div>My Bids</div>
+                        </li>
+                        <li>
+                            <div>Bid Requests</div>
+                        </li>
+                        <li className='mt-2'>
+                            <button className='bg-gray-200 block text-center'>Logout</button>
+                        </li>
+                    </ul>
+                </div>}
             </div>
         </div>
     )
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:
+      [
+
+      ]
     },
   ]);
 
