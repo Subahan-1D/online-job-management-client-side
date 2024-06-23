560716862a7b8ceed453417afc6f1dacf1ce65fd
Subahan-1D <subahanislam523@gmail.com>
2024-06-23 23:45:46 +0600

    Navbar

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,11 @@
+import Navbar from "../components/Navbar";
 
 
 const Main = () => {
     return (
         <div>
             {/* Navbar */}
-            <h1 className="text-3xl font-lato text-center mt-40">Welcome to solosphere session</h1>
+             <Navbar></Navbar>
 
             {/* Outlet */}
 
