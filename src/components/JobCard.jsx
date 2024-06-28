8f46275b16c10f04c398c6deb44ec43ac5c046cc
subahan ali <subahanislam523@gmail.com>
2024-06-28 12:00:48 -0700

    add job

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -5,10 +5,13 @@ import { Link } from "react-router-dom";
 const JobCard = ({job}) => {
   const {description, title,category, min_price, max_price, _id,deadline} = job || {}
   return (
-    <Link to={`/job/${_id}`} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all">
+    <Link
+      to={`/job/${_id}`}
+      className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all"
+    >
       <div className="flex items-center justify-between">
         <span className="text-xs font-light text-gray-800 ">
-          Deadline: {deadline}
+          Deadline: {new Date(deadline).toLocaleDateString()}
         </span>
         <span className="px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full ">
           {category}
@@ -18,8 +21,8 @@ const JobCard = ({job}) => {
       <div>
         <h1 className="mt-2 text-lg font-semibold text-gray-800 ">{title}</h1>
 
-        <p  title={description} className="mt-2 text-sm text-gray-600 ">
-          {description.substring(0,70)}...
+        <p title={description} className="mt-2 text-sm text-gray-600 ">
+          {description.substring(0, 70)}...
         </p>
         <p className="mt-2 text-sm font-bold text-gray-600 ">
           Range: ${min_price} - ${max_price}
--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,5 +1,51 @@
-
+import { useContext, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../provider/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 const AddJob = () => {
+      const [startDate, setStartDate] = useState(new Date());
+      const navigate = useNavigate()
+      const {user} = useContext(AuthContext);
+    const handleFromSubmit = async (e) => {
+      // if(user?.email === email) return  toast.error('Action not permitted')
+      e.preventDefault();
+      const from = e.target;
+      const job_title = from.job_title.value;
+      const email = from.email.value;
+      const deadline = startDate;
+      const category = from.category.value;
+      const min_price = parseFloat(from.min_price.value);
+      const max_price = parseFloat(from.max_price.value);
+      const description = from.description.value;
+      const jobData = {
+        job_title,
+        email,
+        deadline,
+        category,
+        min_price,
+        max_price,
+        description,
+        buyer: {
+          email,
+          name: user?.displayName,
+          photo: user?.photoURL,
+        },
+      };
+      try {
+        const { data } = await axios.post(
+          `${import.meta.env.VITE_APP_URL}/job`,
+          jobData
+        );
+        console.log(data);
+        toast.success("Job Data Updated Successful");
+        navigate("/my-posted-jobs");
+      } catch (err) {
+        console.log(err);
+      }
+    };
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-306px)] my-12">
       <section className=" p-2 md:p-6 mx-auto bg-white rounded-md shadow-md ">
@@ -7,7 +53,7 @@ const AddJob = () => {
           Post a Job
         </h2>
 
-        <form>
+        <form onSubmit={handleFromSubmit}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label className="text-gray-700 " htmlFor="job_title">
@@ -30,6 +76,7 @@ const AddJob = () => {
                 type="email"
                 name="email"
                 disabled
+                defaultValue={user?.email}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
             </div>
@@ -37,6 +84,11 @@ const AddJob = () => {
               <label className="text-gray-700">Deadline</label>
 
               {/* Date Picker Input Field */}
+              <DatePicker
+                className="border p-2 rounded-md"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+              />
             </div>
 
             <div className="flex flex-col gap-2 ">
